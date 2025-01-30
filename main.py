94d17e09003e5f00aef04239b9ad660d15b4b390
Robert Wendel <134088034+robertwendel7@users.noreply.github.com>
2025-01-30 18:14:53 +0000

    segundo commoit

--- main.py
+++ main.py
@@ -7,20 +7,41 @@ app = Flask(__name__)
 #a função sempre precisa retonar algo (String, pagina html, etc)
 @app.route("/")
 def principal():
-    titulo_pagina = "Gestão de Usuários"
-    usuarios = [
-        {"nome" : "Alex", "membro_ativo": True },
-        {"nome" : "Pedrin", "membro_ativo": False}
-    ]
-    return render_template("index.html", titulo = titulo_pagina, lista_usuarios = usuarios)
+    return render_template("cadastro.html")
 
-#
-@app.route("/rotaExtra", methods=['GET', 'POST', 'PUT'])
-def pagina_extra():
-    return """
-        <p> Testando HTML</p>
-        <h1>Testando</h1>
-    """
+
+
+usuarios = []
+@app.route("/cadastrar", methods=['GET', 'POST'])
+def cadastrar():
+    
+    global usuarios
+    nome = request.form.get("nomeUsuario")
+    email = request.form.get("emailUsuario")
+    senha = request.form.get("senhaUsuario")
+    usuarios.append([nome, email, senha])
+    print(usuarios)
+
+    return render_template("login.html")
+
+@app.route("/verificarLogin", methods=['GET', 'POST'])
+def login():
+    titulo_pagina = "Login de Usuários"
+    global usuarios
+    nome = request.form.get("nomeUsuario")
+    email = request.form.get("emailUsuario")
+    senha = request.form.get("senhaUsuario")
+
+    for i in usuarios :
+        if(i.nome == nome and i.email == email and i.senha == senha):
+            mensagem = "Login com sucesso!"
+            return render_template("login.html", mensagem)
+        else:
+            mensagem = "Esse usuário nao está cadastrado ;-;"
+            return render_template("login.html", mensagem)
+
+
+    return render_template("login.html", titulo = titulo_pagina)
 
 #execução do servidor (chamar a função responsavel por executar o servidor web)
 #colocar no modo servidor (recarregar o srvidor web automaticamente)
