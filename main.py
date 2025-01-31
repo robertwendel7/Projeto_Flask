0a76e780c01092923d2356f77fb6410bf0fe9c78
Robert Wendel <134088034+robertwendel7@users.noreply.github.com>
2025-01-31 18:01:28 +0000

    terceiro commit

--- main.py
+++ main.py
@@ -3,45 +3,57 @@ from flask import *
 #inicialização do flask (instanciar o servidor flask)
 app = Flask(__name__)
 
+usuarios = []
+
 #criar uma função atrelada a rota (a ser quando a rota receber uma requisição)
 #a função sempre precisa retonar algo (String, pagina html, etc)
 @app.route("/")
 def principal():
+    return render_template("index.html")
+
+@app.route("/paginaCadastro")
+def paginaCadastro():
     return render_template("cadastro.html")
 
 
-
-usuarios = []
-@app.route("/cadastrar", methods=['GET', 'POST'])
+@app.route("/cadastrarUsuario", methods=['GET', 'POST'])
 def cadastrar():
     
     global usuarios
     nome = request.form.get("nomeUsuario")
     email = request.form.get("emailUsuario")
-    senha = request.form.get("senhaUsuario")
+    senha = str(request.form.get("senhaUsuario"))
     usuarios.append([nome, email, senha])
     print(usuarios)
+    mensagem = "Usuário cadastrado com sucesso"
 
-    return render_template("login.html")
+    return render_template("index.html", mensagem = mensagem)
 
-@app.route("/verificarLogin", methods=['GET', 'POST'])
+@app.route("/verificarLogin", methods=['POST'])
 def login():
-    titulo_pagina = "Login de Usuários"
+
     global usuarios
-    nome = request.form.get("nomeUsuario")
     email = request.form.get("emailUsuario")
-    senha = request.form.get("senhaUsuario")
+    senha = str(request.form.get("senhaUsuario"))
+    logado = False
+    
+    for usuario in usuarios:
+        if(usuario[1] == email and usuario[2] == senha):
+            logado = True
+    if(logado == True):
+        return render_template("logado.html")
+    else:
+        mensagem = "Usuário ou senha incorretos"
+        return render_template("index.html", mensagem = mensagem)
+ 
+    
 
-    for i in usuarios :
-        if(i.nome == nome and i.email == email and i.senha == senha):
-            mensagem = "Login com sucesso!"
-            return render_template("login.html", mensagem)
-        else:
-            mensagem = "Esse usuário nao está cadastrado ;-;"
-            return render_template("login.html", mensagem)
 
+    return render_template("index.html")
 
-    return render_template("login.html", titulo = titulo_pagina)
+@app.route("/paginaAlterarSenha", methods=['POST'])
+def paginaAlterarSenha():
+    return render_template("alterarSenha.html")
 
 #execução do servidor (chamar a função responsavel por executar o servidor web)
 #colocar no modo servidor (recarregar o srvidor web automaticamente)
